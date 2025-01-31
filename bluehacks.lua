8b0dce89049a2a55f57df6caa320ca7c3bb075de
codingiexxpres <jbot8665@gmail.com>
2025-01-31 16:57:34 +0100

    bluehacksv2.lua

--- bluehacks.lua
+++ bluehacks.lua
@@ -77,15 +77,6 @@ local Slider = Tab:CreateSlider({
     end,
  })
 
- local Label = Tab:CreateLabel("made by anti_cheat95", 4483362458, Color3.fromRGB(255, 255, 255), false) -- Title, Icon, Color, IgnoreTheme
 
 
 
-local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
-
-local Button = Tab:CreateButton({
-   Name = "say hi in devconsole",
-   Callback = function(print("hi")
-   -- The function that takes place when the button is pressed
-   end,
-})
