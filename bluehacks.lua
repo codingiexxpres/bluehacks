6f35da96a1385e93e3eaab993db17d00a4f2a6d2
codingiexxpres <jbot8665@gmail.com>
2025-01-31 17:25:52 +0100

    bluehacks.lua

--- bluehacks.lua
+++ bluehacks.lua
@@ -67,32 +67,3 @@ local Slider = Tab:CreateSlider({
     end,
  })
 
- local Keybind = Tab:CreateKeybind({
-    Name = "Keybind",
-    CurrentKeybind = "Q",
-    HoldToInteract = false,
-    Flag = "Keybind1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-    Callback = function(Keybind)
-    -- The function that takes place when the keybind is pressed
-    -- The variable (Keybind) is a boolean for whether the keybind is being held or not (HoldToInteract needs to be true)
- })
-
-local Button = MainTab:CreateButton({
-   Name = "fly",
-   Callback = function()
-        loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")
-   end,
-})
-
-local Slider = MainTab:CreateSlider({
-   Name = "Walkspeed Slider",
-   Range = {0, 550},
-   Increment = 1,
-   Suffix = "Speed",
-   CurrentValue = 16,
-   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.Walkspeed = (Value)
-   end,
-})
-
